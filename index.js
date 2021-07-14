7f8c8ed592635ec4aa533abcfe1bf279381d8e54
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-14 10:58:22 +0200

    Fisrt commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const sort = require('./merge');
+const fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf8', (error, data) => {
+    if (error) {
+        console.log(error.message);
+        return;
+    }
+    const strArr = data.split(' ');
+    const dataArr = strArr.map(x => Number.parseInt(x, 10));
+    console.log(dataArr);
+    const sorter = new sort(dataArr);
+    console.log(dataArr);
+})
