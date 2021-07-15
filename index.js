054175f485d045b874669243db63f507945587a4
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-15 09:37:39 +0200

    Modified try catch statement

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const fs = require('fs');
 const sort = require('./merge');
 const fileName = process.argv[2];
 
-fs.readFile(fileName, 'utf8', (error, data) => {
-    if (error) {
-        console.log(error.message);
-        return;
-    }
-    const strArr = data.split(' ');
-    const dataArr = strArr.map(x => Number.parseInt(x, 10));
-    console.log(dataArr);
-    const sorter = new sort(dataArr);
-    console.log(dataArr);
-})
+try{
+    fs.readFile(fileName, 'utf8', (error, data) => {
+        if (error) {
+            console.log(error.message);
+            return;
+        }
+        const strArr = data.split(' ');
+        const dataArr = strArr.map(x => Number.parseInt(x, 10));
+        console.log(dataArr);
+        const sorter = new sort(dataArr);
+        console.log(dataArr);
+    })
+} catch (error){
+    console.log(error.message)
+}
